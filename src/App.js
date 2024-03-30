7a61b778d1cda895ba0ecf5d702cbed7d6672c2e
ym1021 <ym1021@cu.ac.kr>
2024-03-31 00:32:25 +0900

    linked front and back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/main/example")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) =>
+        console.error("There was a problem with your fetch operation:", error)
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{message}</p>
     </div>
   );
 }
